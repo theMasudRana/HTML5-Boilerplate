76ef07a39a6753e0597ecbd040ffad80ee6cb3f1
theMasudRana <mr.masudrana00@gmail.com>
2017-09-21 20:50:49 +0600

    Swiper slider added

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,14 @@
         scrollContainer: null
     });
     wowInit.init();
-
+    
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        parallax: true,
+        speed: 600,
+    });
 
 })(jQuery)
