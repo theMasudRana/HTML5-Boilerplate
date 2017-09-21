c9fdb23bd903973e4a8f0124543aba53d749a651
theMasudRana <mr.masudrana00@gmail.com>
2017-09-21 17:12:01 +0600

    Styles updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@
     'use strict';
 
     // All JavaScript pluging initialization code here
-    
-	// WoW js init
-	new WOW().init();
+
+    // WoW js init
+    var wowInit = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated', 
+        offset: 0, 
+        mobile: true, 
+        scrollContainer: null
+    });
+    wowInit.init();
 
 
 })(jQuery)
