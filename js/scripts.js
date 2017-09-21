7d837e80652b647df57c957444071706b805ef04
theMasudRana <mr.masudrana00@gmail.com>
2017-09-21 21:12:45 +0600

    Favicon changed

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,12 @@
     // WoW js init
     var wowInit = new WOW({
         boxClass: 'wow',
-        animateClass: 'animated', 
-        offset: 0, 
-        mobile: true, 
+        animateClass: 'animated',
+        offset: 0,
+        mobile: true,
         scrollContainer: null
     });
     wowInit.init();
-    
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        parallax: true,
-        speed: 600,
-    });
+
 
 })(jQuery)
